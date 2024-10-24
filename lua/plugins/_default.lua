f244fc074640a1339eb9fbfb53d6b312113bad2b
Pojok Code <pojokcodeid@gmail.com>
2024-10-24 09:50:54 +0700

    enc: update notify error alert for toggleterm

--- lua/plugins/_default.lua
+++ lua/plugins/_default.lua
@@ -8,6 +8,11 @@ _G.idxOf = function(array, value)
 end
 
 _G.LAZYGIT_TOGGLE=function ()
+  local ok = pcall(require,"toggleterm")
+  if not ok then
+    require("notify")("toggleterm not found!", "error")
+    return
+  end
   local Terminal = require("toggleterm.terminal").Terminal
   local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
   lazygit:toggle()
