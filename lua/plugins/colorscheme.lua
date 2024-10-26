ce0832a925ac7b61add6fa7f4119e5ae50028f10
Pojok Code <pojokcodeid@gmail.com>
2024-10-26 14:47:12 +0700

    enc: overide eva theme mini indent scope hl

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,4 +2,16 @@ return {
 	"sharpchen/Eva-Theme.nvim",
 	lazy = false,
 	priority = 1000,
+	build = "EvaCompile",
+	opts = {
+		override_highlight = {
+			dark = {
+				["MiniIndentscopeSymbol"] = { fg = "#FF9070" },
+				["MiniIndentscopeSymbolOff"] = { fg = "#FF9070" },
+			},
+		},
+	},
+	config = function(_, opts)
+		require("Eva-Theme").setup(opts)
+	end,
 }
