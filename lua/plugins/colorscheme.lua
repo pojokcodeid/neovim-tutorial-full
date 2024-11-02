e7b343fa910a88dfe51e6994b24c3c009b64decb
Pojok Code <pojokcodeid@gmail.com>
2024-11-02 15:19:57 +0700

    enc: update config windbar navic

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,14 +3,20 @@ return {
 	lazy = false,
 	priority = 1000,
 	build = ":EvaCompile",
-	opts = {
-		override_highlight = {
+	opts = function(_, opts)
+		local dark = require("Eva-Theme.palette"):from_variant("dark")
+		opts.override_highlight = {
 			dark = {
 				["MiniIndentscopeSymbol"] = { fg = "#FF9070" },
 				["MiniIndentscopeSymbolOff"] = { fg = "#FF9070" },
+				["WinBar"] = { bg = dark.background },
+				["WinBarNC"] = { bg = dark.background },
+				["StatusLine"] = { bg = dark.background },
+				["StatusLineNC"] = { bg = dark.background },
 			},
-		},
-	},
+		}
+		return opts
+	end,
 	config = function(_, opts)
 		require("Eva-Theme").setup(opts)
 		local colorscheme = "Eva-Dark"
