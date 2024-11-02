4b52245b000f834ed0a607f8417063208c80c564
Pojok Code <pojokcodeid@gmail.com>
2024-11-02 15:09:52 +0700

    enc: custem hilight for navic

--- lua/plugins/extras/navic.lua
+++ lua/plugins/extras/navic.lua
@@ -30,6 +30,14 @@ function M.config()
 			return text
 		end,
 	})
+	local ready, color = pcall(require, "Eva-Theme.palette")
+	if ready then
+		local hi = vim.api.nvim_set_hl
+		hi(0, "WinBar", { bg = color.background })
+		hi(0, "WinBarNC", { bg = color.background })
+		hi(0, "StatusLine", { bg = color.background })
+		hi(0, "StatusLineNC", { bg = color.background })
+	end
 end
 
 return M
