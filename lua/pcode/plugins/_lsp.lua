c30835cc4189067a53c4643fd22b0549e7289045
Asep Komarudin <68836805+pojokcodeid@users.noreply.github.com>
2024-11-04 00:22:56 +0700

    enc: update config border lsp info

--- lua/pcode/plugins/_lsp.lua
+++ lua/pcode/plugins/_lsp.lua
@@ -12,9 +12,9 @@ return {
 					"LspInstall",
 					"LspUninstall",
 				},
-				-- config = function()
-				-- 	require("lspconfig.ui.windows").default_options.border = "rounded"
-				-- end,
+				config = function()
+					require("lspconfig.ui.windows").default_options.border = "rounded"
+				end,
 			},
 			{
 				"williamboman/mason.nvim",
